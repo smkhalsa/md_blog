17c2586e9c789264ce54c1db3b63f8354f5809f9
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-06-12 23:26:06 -0700

    Add posts to firebase

--- app/posts/posts.js
+++ app/posts/posts.js
@@ -1,5 +1,7 @@
-angular.module('app.posts', [])
+angular.module('app.posts', ['firebase'])
   
-  .controller('PostsController', function($scope) {
-    
+  .controller('PostsController', function($scope, $firebaseArray) {
+    var ref = new Firebase("https://satkhalsa.firebaseio.com/posts");
+
+    $scope.posts = $firebaseArray(ref);
   })
